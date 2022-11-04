ff86775ba5780f4faceb28d8bfea08f2f52784ae
Janaka Gayanath <janakagayanath@gmail.com>
2022-11-04 20:58:29 +0530

    button add

--- lib/screens/sing_in.dart
+++ lib/screens/sing_in.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grocery/components/customtextfield.dart';
+import 'package:grocery/utils/app_colors.dart';
 import 'package:grocery/utils/assets_constant.dart';
 
 import '../components/custom_text.dart';
@@ -23,14 +24,14 @@ class _LoginState extends State<Login> {
       width: size.width,
       child: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 96.0,
           ),
-          Custom_Text(
+          const Custom_Text(
             "Login",
             fontSize: 25.0,
           ),
-          SizedBox(
+          const SizedBox(
             height: 41,
           ),
           Image.asset(
@@ -38,14 +39,41 @@ class _LoginState extends State<Login> {
             width: 202,
             height: 138,
           ),
-          SizedBox(
+          const SizedBox(
             height: 45,
           ),
           CustomTextfiled(controller: _email, hintText: "Email"),
-          SizedBox(
+          const SizedBox(
             height: 8,
           ),
           CustomTextfiled(controller: _password, hintText: "Password",isobscure: true,),
+          const SizedBox(
+            height: 16,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: InkWell(
+              onTap: (){
+                // redirect forgot password screen
+              },
+              child: Custom_Text(
+                "Forgot your Password?",fontSize: 14,color: Colors.black,fontWeight: FontWeight.w500,),
+            ),
+          ),
+          const SizedBox(
+            height: 29,
+          ),
+          Container(
+            width: 259,
+            height: 50,
+            decoration: BoxDecoration(
+              color: AppColors.prymeryColor,
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+            ),
+            child: Align(
+              alignment: Alignment.center,
+                child: Custom_Text("Login",color: Colors.white,fontSize: 18.0,fontWeight: FontWeight.w500,)),
+          ),
         ],
       ),
     ));
